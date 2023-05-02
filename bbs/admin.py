c24db1b8643bb8b0eff9f902a2d170d0226b1f33
fusako_n <98594715+Fusako-n@users.noreply.github.com>
2023-05-02 17:59:35 +0900

    edited 0502

--- bbs/admin.py
+++ bbs/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from .models import Topic, Category, Contacts
 from .forms import TopicAdminForm
-from django_summernote.admin import SummernoteModelAdmin
 
 
 class TopicAdmin(admin.ModelAdmin):
     list_display = ['id', 'comment', 'name', 'created_at']
     list_filter = ['name']
     form = TopicAdminForm
-
 
 admin.site.register(Topic, TopicAdmin)
 admin.site.register(Category)
